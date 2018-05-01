3f917c35e43cd4a21bac9ecdf7f40006856b71ec
Hank Wang <drapho@gmail.com>
2018-05-02 05:03:05 +0900

    add basic Outlet and Action

--- PitchPerfetct/ViewController.swift
+++ PitchPerfetct/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+    }
 }
 
